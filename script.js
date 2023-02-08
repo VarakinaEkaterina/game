43ef8548adc20c8c4f3bd82825708715a75db04e
Kate Varakina <98549302+VarakinaEkaterina@users.noreply.github.com>
2023-02-08 22:16:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,19 +8,42 @@ function turnoff() {
     document.getElementById('barrier2').style.animationPlayState = "paused";
 }
 
-document.addEventListener('keydown',  direction);
+document.addEventListener('keydown',  moveBird);
 let posbird = 310;
 
-function direction(event){
-    if (event.keyCode == 38 ) {
-        posbird -= 30;
-        posbird += "px"   
-        document.getElementById('bird').style.top = posbird;
-    }
-
-    else if (event.keyCode == 40 ) {
-        posbird += 30;
-        posbird += "px"   
-        document.getElementById('bird').style.top = posbird;
+function moveBird(event){
+    switch (event.keyCode) {
+        case 38:
+            if (posbird > 0){
+                posbird -= 25;
+                document.getElementById('bird').style.top = `${posbird}px`;
+            }
+            else {
+                posbird -= 0;
+                document.getElementById('bird').style.top = `${posbird}px`;  
+            }
+            break;
+        case 40:
+            if (posbird < 530){
+                posbird += 25;
+                document.getElementById('bird').style.top = `${posbird}px`;
+            }
+            else {
+                posbird += 0;
+                document.getElementById('bird').style.top = `${posbird}px`;  
+            }
+            break;
     }
 } 
+
+let isAlive = setInterval (function(){
+    let birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+    let barrier1Left = parseInt(window.getComputedStyle(barrier1).getPropertyValue("left"));
+    let barrier2Left = parseInt(window.getComputedStyle(barrier2).getPropertyValue("left"));
+
+    if (barrier1Left < 50 && barrier1Left > 0 && birdTop >= 250 || barrier2Left < 50 && barrier2Left > 0 && birdTop <= 250) {
+        alert("game over")
+        
+    }
+},10)
+
