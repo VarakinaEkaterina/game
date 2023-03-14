43519e4f0689f181629a67622bfa1faa724f1d9e
Kate Varakina <98549302+VarakinaEkaterina@users.noreply.github.com>
2023-03-14 21:53:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -36,14 +36,35 @@ function moveBird(event){
     }
 } 
 
-let score = 0;
+// function position() {
+//     let barrier1Left = parseInt(window.getComputedStyle(barrier1).getPropertyValue("left"));
+//     let barrier2Left = parseInt(window.getComputedStyle(barrier2).getPropertyValue("left"));
+//     if (barrier1Left >= 900 && barrier1Left <= 999) { 
+//         let randomNumber = Math.floor(Math.random() * 10) + 1;
+//         let barrier1Top = parseInt(window.getComputedStyle(barrier1).getPropertyValue("top"));
+//         let height1 = randomNumber*10+barrier1Top;
+//         document.getElementById('barrier1').style.top = `${height1}px`;
+//     }
+//     else if(barrier2Left >= 900 && barrier2Left <= 999){
+//         let randomNumber = Math.floor(Math.random() * 10) + 1;
+//         let barrier2Bottom = parseInt(window.getComputedStyle(barrier2).getPropertyValue("bottom"));
+//         let height1 = randomNumber*10+barrier2Bottom;
+//         document.getElementById('barrier1').style.bottom = `${height1}px`;
+//     }
 
+// }
+// setInterval(position, 100); 
+
+
+let score = 0;
 let isAlive = setInterval (function(){
-    let birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
     let barrier1Left = parseInt(window.getComputedStyle(barrier1).getPropertyValue("left"));
     let barrier2Left = parseInt(window.getComputedStyle(barrier2).getPropertyValue("left"));
-
-    if (barrier1Left < 50 && barrier1Left > 0 && birdTop >= 250 || barrier2Left < 50 && barrier2Left > 0 && birdTop <= 250) {
+    let birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+    let barrier1Top = parseInt(window.getComputedStyle(barrier1).getPropertyValue("top"));
+    let barrier2Bottom = parseInt(window.getComputedStyle(barrier2).getPropertyValue("bottom"));
+    
+    if (barrier1Left < 50 && barrier1Left > 0 && birdTop >= barrier1Top || barrier2Left < 50 && barrier2Left > 0 && birdTop <= (barrier2Bottom-100)) {
         document.getElementById('game').style.backgroundImage = "none";
         document.getElementById('barrier1').style.display = "none";
         document.getElementById('barrier2').style.display = "none";
@@ -56,9 +77,11 @@ let isAlive = setInterval (function(){
             location.reload()
         }
     }
-    else if (barrier1Left < 50 && barrier1Left > 0 && birdTop <= 250 || barrier2Left < 50 && barrier2Left > 0 && birdTop >= 250) {
+    else if (barrier1Left < 50 && barrier1Left > 0 && birdTop <= barrier1Top || barrier2Left < 50 && barrier2Left > 0 && birdTop >= (barrier1Top-100)) {
         score++;
         document.getElementById("score1").innerHTML = score;
     }
 },200)
+
+
 
