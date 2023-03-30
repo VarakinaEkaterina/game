4e98d85561eca9c8bbc517da92e8bc68bc0bf80d
Kate Varakina <98549302+VarakinaEkaterina@users.noreply.github.com>
2023-03-30 13:26:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 function turnon() {
-  // setInterval(position, 5500);
+  setInterval(position, 5500);
   document.getElementById("barrier1").style.animationPlayState = "running";
   document.getElementById("barrier2").style.animationPlayState = "running";
 }
+//кнопка старта
 
 function turnoff() {
-  // clearInterval(position);
+  clearInterval(position);
   document.getElementById("barrier1").style.animationPlayState = "paused";
   document.getElementById("barrier2").style.animationPlayState = "paused";
 }
+//кнопка пауза
 
 document.addEventListener("keydown", moveBird);
 let posbird = 310;
+//считывает нажатие кнопок
 
 function moveBird(event) {
   switch (event.keyCode) {
@@ -19,34 +22,40 @@ function moveBird(event) {
       if (posbird > 0) {
         posbird -= 25;
         document.getElementById("bird").style.top = `${posbird}px`;
+        //движение вверх
       } else {
         posbird -= 0;
         document.getElementById("bird").style.top = `${posbird}px`;
+        //граница поля
       }
       break;
     case 40:
       if (posbird < 530) {
         posbird += 25;
         document.getElementById("bird").style.top = `${posbird}px`;
+        //движение вниз
       } else {
         posbird += 0;
         document.getElementById("bird").style.top = `${posbird}px`;
+        //граница поля
       }
       break;
   }
 }
 
-// function position() {
-//   const barrier1 = document.querySelector("#barrier1");
-//   const barrier2 = document.querySelector("#barrier2");
-//   let barrier1Top = getRandomeNumber(240, 380);
-//   let barrier2Bottom = getRandomeNumber(370, 525);
-//   barrier1.style.top = `${barrier1Top}px`;
-//   barrier2.style.bottom = `${barrier2Bottom}px`;
-// }
-// function getRandomeNumber(min, max) {
-//   return Math.round(Math.random() * (max - min) + min);
-// }
+function position() {
+  const barrier1 = document.querySelector("#barrier1");
+  const barrier2 = document.querySelector("#barrier2");
+  let barrier1Top = getRandomeNumber(240, 380);
+  let barrier2Bottom = getRandomeNumber(370, 525);
+  barrier1.style.top = `${barrier1Top}px`;
+  barrier2.style.bottom = `${barrier2Bottom}px`;
+}
+//расположение деревьев
+  function getRandomeNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+//функция рандома для деревьев
 
 let score = 0;
 let isAlive = setInterval(function () {
@@ -63,7 +72,7 @@ let isAlive = setInterval(function () {
   let barrier2Bottom = parseInt(
     window.getComputedStyle(barrier2).getPropertyValue("bottom")
   );
-
+//получение данных о положении деревьев и птицы
   if (
     (barrier1Left < 50 && barrier1Left > 0 && birdTop >= barrier1Top) ||
     (barrier2Left < 50 && barrier2Left > 0 && birdTop <= barrier2Bottom - 100)
@@ -76,11 +85,13 @@ let isAlive = setInterval(function () {
     document.getElementById(
       "game"
     ).innerHTML = `${"<br>"}Game over! ${"<br>"} You score: ${score}`;
-
+    
+//изменение экрана при проигрыше
     setTimeout(reload, 3000);
     function reload() {
       location.reload();
     }
+ //перезагрузка страницы
   } else if (
     (barrier1Left < 50 && barrier1Left > 0 && birdTop <= barrier1Top) ||
     (barrier2Left < 50 && barrier2Left > 0 && birdTop >= barrier1Top - 100)
@@ -89,3 +100,4 @@ let isAlive = setInterval(function () {
     document.getElementById("score1").innerHTML = score;
   }
 }, 200);
+//счетчик игры
